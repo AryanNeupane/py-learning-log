6a2a055c7e0543ae097e55ee125d7d2c4b35ce5b
Aryan Neupane <official.aryanneupane@gmail.com>
2025-05-01 11:32:00 +0545

    python learning

---
+++ func.py
@@ -0,0 +1,32 @@
+# print("hello World")
+
+# def sum(a,b,c):
+#     return a+b+c
+# print(sum(2,3,5))
+
+
+# def greet(name,greet="Thank You"):
+#     print(f"hello {name}")
+#     print(greet)
+
+# greet("ram","Good Day")
+
+# def fact(n):
+#     if(n==1 or n==0):
+#         return 1
+#     return n*fact(n-1)
+
+# print(fact(3))
+
+
+def grt(a,b,c):
+    if(a>b and a>c):
+        print(f"{a} is the greatest")
+    elif(b>a and b>c):
+        print(f"{b}is the greatest num")
+    else:
+        print(f"{c} is the greatest number")
+
+
+# grt(367,48,95)
+
