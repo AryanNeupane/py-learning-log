d03ff2284bbcbe97244dce3d78d8b944706224d7
Aryan Neupane <official.aryanneupane@gmail.com>
2025-05-06 00:11:35 +0545

    arr properties

---
+++ num2.py
@@ -0,0 +1,21 @@
+
+import numpy as np
+
+### vector , Matrix and Tensor
+
+vec=np.array([1,2,3,4])
+# print(vec)
+
+
+### 2D =====> matrix
+
+matrix=np.array([[1,3,5,7],[2,4,6,8]])
+# print(matrix)
+
+import numpy as np
+
+tensor = np.array([[[[1, 2], [2, 3]],
+                    [[2, 2], [3, 3]]]])
+
+
+print(tensor)
---
+++ num3.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+### arryay attributes
+
+matrix=np.array([[1,3,5,7],[2,4,6,8]])
+# print(matrix.shape)
+# print(matrix.ndim)
+# print(matrix.size)
+# print(matrix.dtype)
+
+
+
+### array RESHAPING
+arr= np.arange(12)
+rearr=arr.reshape((3,4))
+print(rearr)
