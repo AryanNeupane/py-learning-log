a26cdbcaf88cd136c995c349d3426e4e13875d95
Aryan Neupane <official.aryanneupane@gmail.com>
2025-05-05 23:50:30 +0545

    numpy

--- num1.py
+++ num1.py
@@ -1,6 +1,26 @@
 import numpy as np
 
 num_array=np.array([1,2,3,4,5])
-print(num_array)
+# print(num_array)
 num_array2=np.array([[1,2,3,4,5],[1,2,3,4,5]])
-print(num_array2)
+# print(num_array2)
+
+
+# py_list=[2,3,4,5]
+# print(2*py_list)
+
+# print(num_array*2)
+
+
+# np_array_range=np.arange(2,30)
+# print(np_array_range)
+
+
+# Zeros 
+
+zeros=np.zeros((3,5))
+print(zeros)
+
+#Ones
+ones=np.ones((2,2))
+print(ones)
