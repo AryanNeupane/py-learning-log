5689d3d3bd9f89fa8353e0512e68a6d2cbcde4c8
Aryan Neupane <official.aryanneupane@gmail.com>
2025-05-05 23:55:39 +0545

    numpy fnction

--- num1.py
+++ num1.py
@@ -6,21 +6,36 @@ num_array2=np.array([[1,2,3,4,5],[1,2,3,4,5]])
 # print(num_array2)
 
 
-# py_list=[2,3,4,5]
+py_list=[2,3,4,5]
 # print(2*py_list)
 
 # print(num_array*2)
 
 
-# np_array_range=np.arange(2,30)
+np_array_range=np.arange(2,30)
 # print(np_array_range)
 
 
 # Zeros 
 
 zeros=np.zeros((3,5))
-print(zeros)
+# print(zeros)
 
 #Ones
 ones=np.ones((2,2))
-print(ones)
+# print(ones)
+
+
+#full
+
+full=np.full((2,3),7)
+# print(full)
+
+
+
+random=np.random.random((2,3))
+# print(random)
+
+
+seq=np.arange(0,11,2)
+# print(seq)
